31199467f5e744d1322e49cba8a2124bebfb97fe
Anuj Singh <38321477+anujsingh-088@users.noreply.github.com>
2021-10-12 01:36:31 +0530

    Create swapPairs.js

---
+++ swapPairs.js
@@ -0,0 +1,52 @@
+/*
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head, prev = null, length = 0) {
+    if(!head)
+        return null;
+    length++;
+    if(length % 2 && head.next){ //prev 2
+        var nextMe = head.next; // 4
+        
+        head.next = swapPairs(head.next, head, length); // 1->null
+        nextMe.next = head;
+        return nextMe
+    } else if(length % 2)
+        return head
+    head.next = swapPairs(head.next, head, length);
+    return head.next;
+};
