4265c0fdc060e8d1890e39c7893d3ccf8a7e1e03
Anuj Singh <38321477+anujsingh-088@users.noreply.github.com>
2021-11-14 23:41:32 +0530

    Create groupAnagrams.js

---
+++ groupAnagrams.js
@@ -0,0 +1,55 @@
+/*
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    var obj = {};
+    var list = [];
+    for(let i=0;i<strs.length;i++){
+        let str = sortString(strs[i])
+        if(obj[str] && obj[str].length)
+            obj[str].push(strs[i]);
+        else{
+            obj[str] = [];
+            obj[str].push(strs[i]);
+        }
+    }
+    for(let i in obj){
+        list.push(obj[i])
+    }
+    return list
+};
+
+function sortString(str){
+  var arr = str.split('');
+  var sorted = arr.sort();
+  return sorted.join('');
+}
