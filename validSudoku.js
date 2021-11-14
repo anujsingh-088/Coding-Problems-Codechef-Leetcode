8fd962ee8f849d78d39cbe0baf867d01976c6f25
Anuj Singh <38321477+anujsingh-088@users.noreply.github.com>
2021-11-14 23:37:33 +0530

    Create validSudoku.js

---
+++ validSudoku.js
@@ -0,0 +1,126 @@
+/*
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    return validateRows(board) && validateColumns(board) && validateMatrix(board)
+};
+
+function validateRows(board){
+    
+    for(let i=0;i<board.length;i++){
+        for(let j=0;j<board[i].length;j++){
+            if(board[i][j] ==  ".")
+               continue;
+            else if(board[i].lastIndexOf(board[i][j]) != j){
+                return false
+            }
+                
+        }
+    }
+    return true;
+}
+
+function validateColumns(board){
+    let m = board.length;
+    let n = board[0].length
+    
+     for(let i=0;i<n;i++){
+         let arr = new Array(9);
+        for(let j=0;j<m;j++){
+            if(board[j][i] ==  ".")
+               continue;
+            else if(arr[board[j][i]]){
+                return false
+            }
+            arr[board[j][i]] = 1;
+        }
+    }
+    return true;
+}
+
+function validateMatrix(board){
+    for(let k=1;k<=3;k++){
+        let arr1 = new Array(9);
+        let arr2 = new Array(9);
+        let arr3 = new Array(9);
+        for(let i=3*(k-1);i<k*3;i++){
+            
+            for(let j=0;j<3;j++){
+                if(board[i][j] == ".")
+                    continue;
+                if(arr1[board[i][j]])
+                    return false;
+                arr1[board[i][j]] = 1;
+            }
+            for(let j=3;j<6;j++){
+                if(board[i][j] == ".")
+                    continue;
+                if(arr2[board[i][j]]){
+                    return false;
+                }
+                 
+                arr2[board[i][j]] = 1;
+            }
+            
+            for(let j=6;j<9;j++){
+                if(board[i][j] == ".")
+                    continue;
+                if(arr3[board[i][j]])
+                    return false;
+                arr3[board[i][j]] = 1;
+            }
+        }
+    }
+    
+    return true
+}
